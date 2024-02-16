9fb5fa7886771562a3ae0359dbb97efeaae63975
Mattia Russo <russo.1918968@studenti.uniroma1.it>
2024-02-16 20:17:41 +0100

    Implemented connection with PythonAnyWhere

--- app/src/main/java/com/example/greenspot/presentation/spotter/addReport/AddReportViewModel.kt
+++ app/src/main/java/com/example/greenspot/presentation/spotter/addReport/AddReportViewModel.kt
@@ -13,7 +13,10 @@ import com.google.firebase.firestore.GeoPoint
 import com.google.firebase.firestore.firestore
 import com.google.firebase.storage.storage
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 
@@ -63,25 +66,23 @@ class AddReportViewModel : ViewModel() {
             .addOnSuccessListener {
                 Log.i("addReport","Report loaded on firestore with success!")
 
-                Log.d("newReports","url: https://ilcameriere.pythonanywhere.com/api/data?id=${it.id}&lat=${location.latitude}&log=${location.longitude}")
+                //Log.d("newReports","url: https://ilcameriere.pythonanywhere.com/api/data?id=${it.id}&lat=${location.latitude}&log=${location.longitude}")
                 //update votes using python anywhere
                 val url = URL("https://ilcameriere.pythonanywhere.com/api/data?id=${it.id}&lat=${location.latitude}&log=${location.longitude}")
+                GlobalScope.launch(Dispatchers.IO) {
+                    with(url.openConnection() as HttpURLConnection) {
+                        requestMethod = "GET"
 
-                with(url.openConnection() as HttpURLConnection) {
-                    requestMethod = "GET"
+                        // Leggi la risposta
+                        BufferedReader(InputStreamReader(inputStream)).use {
+                            val response = StringBuffer()
 
-                    // Leggi la risposta
-                    BufferedReader(InputStreamReader(inputStream)).use {
-                        val response = StringBuffer()
-
-                        var inputLine = it.readLine()
-                        while (inputLine != null) {
-                            response.append(inputLine)
-                            inputLine = it.readLine()
+                            var inputLine = it.readLine()
+                            while (inputLine != null) {
+                                response.append(inputLine)
+                                inputLine = it.readLine()
+                            }
                         }
-
-                        // Stampa la risposta
-                        println(response.toString())
                     }
                 }
 
